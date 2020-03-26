9a0352e583a2d34bc2f7eb55da6d64e21108358c
Faith-Shanny <61988452+Faith-Shanny@users.noreply.github.com>
2020-03-26 22:03:48 +0100

    Add files via upload

---
+++ myjava.js
@@ -0,0 +1,13 @@
+let name = 'faith .c. Ibe';
+console.log("Myname is: " + name);
+let selectedCourses = ('html', 'css', 'javascript')
+console.log(selectedCourses);
+let username = 'faith-shanny';
+console.log(userName);
+const data = [];
+for (let i = 0; i <=200; i++) {
+	if(i%2===1) {
+		data.push(i);
+	}
+}
+console.log(data);
